bdddda80556ae158ae7625ae5ed3828e6cc2222d
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-12-01 14:26:28 +0300

    Update.

--- RefactoringsTest2021/IntroduceParameter.cs
+++ RefactoringsTest2021/IntroduceParameter.cs
@@ -2,17 +2,17 @@
 {
     public class IntroduceParameter
     {
-        public void IntroduceParameter1()
+        public void IntroduceParameter1(string s)
         {
             var introduceParameter = new IntroduceParameter();
             // Introduce here
             var selectSomeThing = introduceParameter.SelectSomeThing.SelectSomeThing.SelectSomeThing.SelectSomeThing.SelectSomeThing;
 
             var v1 = "hello";
-            var v2 = IntroduceParameter2(v1);
+            var v2 = IntroduceParameter2(v1, s);
         }
 
-        static int IntroduceParameter2(string? param) => param!.Length;
+        static int IntroduceParameter2(string? param, string s) => param!.Length;
 
         public IntroduceParameter SelectSomeThing;
 
