5f7db01ec4e3731d821bd63793272f58a96dd0a6
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-07-28 11:42:23 +0200

    Add project with global usings.

---
+++ GlobalUsingsProject/ExtractGlobalUsings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace GlobalUsingsProject;
+
+public class ExtractGlobalUsings
+{
+    public void Test()
+    {
+        
+        var listDictionary = new ListDictionary();
+        var json = new JsonConverterAttribute(typeof(A));
+
+        Console.WriteLine(json);
+    }
+}
+
+public class A
+{
+}
---
+++ GlobalUsingsProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Specialized;
+
+Console.WriteLine("Hello, World!");
+
+var listDictionary = new ListDictionary();
